342b0f4898a88f73e8ca34cd91e69d367b524786
devop <devops.engineering06@gmail.com>
2022-12-22 08:42:50 +0300

    Unnecessary string interpolations

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -10,7 +10,7 @@ class MealDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '$mealId',
+          mealId,
         ),
       ),
       body: Center(
