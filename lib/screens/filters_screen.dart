7eb09e493b06a3ff82d5d9e1449f07cc0747a41b
devop <devops.engineering06@gmail.com>
2022-12-22 23:47:28 +0300

    Replacing Pages (Instead of Pushing)

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   const FiltersScreen({super.key});
   static const routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Filters!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      drawer: const MainDrawer(),
+      body: const Center(
+        child: Text('Filters!'),
+      ),
     );
   }
 }
